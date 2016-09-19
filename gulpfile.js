2f9b64ed5b76d37f9839051cdc5ca43d5a503e47
gurujilove108 <dylan.dannenhauer@gmail.com>
2016-09-19 12:17:01 -0700

    added some functions and cleaned up my master gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ var gulp 		= require('gulp'),
 	/* used for compressing javascript, we can use the words minifying and compresssing interchangeably */ 
 	uglify		= require('gulp-uglify'),
 
-	/* rimraf is used for deleting files, pretty cool huh! */
+	/* rimraf is used for deleting files, folder, and all content of a folder recursively */
 	rimraf 		= require('rimraf'),
 
 	/* used for compressing html */
@@ -36,49 +36,106 @@ var gulp 		= require('gulp'),
 	/* used for renaming files */
 	rename 		= require('gulp-rename'),
 
+	/* used for merging multiple gulp streams into 1 */
 	mergeStream = require('merge-stream'),
 
-	del 		= require('del');
+	/* used to delete files and folders */
+	del 		= require('del'),
 
+	/* used for ignoring certain file patterns when performing gulp tasks */
+	gulpIgnore 	= require('gulp-ignore'),
+
+	/* used for checking conditions when performing gulp tasks, gulp-if just adds the ability to add if statements to our tasks */
+	gulpIf 		= require('gulp-if');
+
+/* process is a global node object and we can access any command line arguments by process.argv, no plugins are required for this */
 var cliArgs = process.argv;
 
-gulp.task("default", function(done) {
-	console.log("Run as many gulp tasks as possible");
+/* One example of copying files. In this case, notice we have a folder test in our root directory */
+gulp.task("copy-files-v1", function(done) {
+
+	/* 1. Add all files recursively in the static folder to the test folder. If test folder does not exist, it will be created */
+	/* 2. Add every python file recursively from the root directory into the test folder */
+	/* 3. Add every txt file to the test folder except in this case we are going to ignore the node_modules folder, if the folder copiedTextFiles does not exist inside of test, it will be created */
+	gulp.src('static/**/*').pipe(gulp.dest('test'));
+	gulp.src('./**/*.py').pipe(gulp.dest('test'));
+	gulp.src('./**/*.txt').pipe(gulpIgnore.exclude("node_modules/**")).pipe(gulp.dest('test/copiedTextFiles'));
+
 	done();
 });
 
-gulp.task("show-cli-args", function(done) {
-	
+/* Do the same thing as above except we are going to use the merge-stream plugin */
+gulp.task("copy-files-v2", function(done) {
+	return mergeStream(
+		gulp.src('static/**/*').pipe(gulp.dest('test')),
+		gulp.src('./**/*.py').pipe(gulp.dest('test')),
+		gulp.src('./**/*.txt').pipe(gulpIgnore.exclude("node_modules/**")).pipe(gulp.dest('test/copiedTextFiles'))
+	);
 });
 
-/* One example of copying files into the test folder*/
-gulp.task("copy-files-v1", function(done) {
-
-	/* Add all files recursively in the static folder to the test folder. If test folder does not exist, it will be created */
-	gulp.src('static/**/*').pipe(gulp.dest('test'));
-
-	/* Add every python file recursively from the root directory into the test folder */
-	gulp.src('**/*.py').pipe(gulp.dest('test'));
-});
-
-/* Use the del plugin to delete a folder, file in this case means a folder or a file*/
+/* 1. Use the del plugin to delete a file */
+/* 2. Use the rimraf plugin to delete a file */
 gulp.task("remove-file-v1", function(done) {
-	del(['test'], done);
+	del(['temp.txt'], done);
 });
 
-/* Use the rimraf plugin to delete all content of a folder recursively and passing in a custom callback */
-gulp.task('remove-folder-content', function(done) {
-	rimraf('/test', function() {
-		done();
-	})
-});
-
-/* Use the rimraf plugin to delete a folder*/
 gulp.task("remove-file-v2", function(done) {
+	rimraf("temp.txt", done);
+});
+
+/* 1. Use the del plugin to delete a folder */
+/* 2. Use the rimraf plugin to delete a folder */
+gulp.task("remove-folder-v1", function(done) {
+
+	/* We don't have to have a return statement, it can be omitted, but the key to the return statement is to make sure asynchronous tasks are done */
+	return del(['test'], done);
+});
+
+gulp.task("remove-folder-v2", function(done) {
 	rimraf('test', done);
 });
 
-/* Use rimraf to delete all contents of a folder */
+/* Use rimraf to delete all contents of a folder, but not the folder itself, and passing in a custom callback */
+gulp.task('remove-folder-contents-v1', function(done) {
+	rimraf('test/*', function() {
+		done();
+	});
+});
+
+/* Same as above except were going to be tricky about how we pass in a callback */
+gulp.task('remove-folder-contents-v2', function(done) {
+	rimraf('test/*', (function() {
+		return function() {
+			done();
+		}
+	})());
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // var ignore = require('gulp-ignore');
 // var rimraf = require('gulp-rimraf');
